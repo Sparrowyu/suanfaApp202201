c948b69a771f02d25ad665d036037a15c503a73f
yutao <yutao@cece.com>
2022-07-28 18:06:17 +0800

    测试

--- app/src/main/java/com/example/suanfaapp202201/Test1.java
+++ app/src/main/java/com/example/suanfaapp202201/Test1.java
@@ -4,4 +4,7 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("This is a test!");
     }
+    public static void main1(String[] args) {
+        System.out.println("测试commit");
+    }
 }
